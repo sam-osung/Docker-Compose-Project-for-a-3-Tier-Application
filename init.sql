954791664aa9f8effac3ebc2988935e9a68d59fc
sam <osung996@gmail.com>
2026-05-08 09:45:23 +0100

    my sql entry point added

---
+++ init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE school;
+
+USE school;
+
+CREATE TABLE student (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(40),
+  roll_number INT,
+  class VARCHAR(16)
+);
+
+CREATE TABLE teacher (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(40),
+  subject VARCHAR(40),
+  class VARCHAR(16)
+);
